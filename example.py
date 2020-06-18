774360b89af91bab8991a0993e19007f4d116fe8
Jona <Jona.fleischmann@gmail.com>
2020-06-18 19:15:33 +0200

    test

--- example.py
+++ example.py
@@ -1,5 +1,8 @@
 import giphyApi
 
+#######################################################################
+############# Version 1.0               Author: DarkNoise #############
+#######################################################################
 
 # Set Api key
 API_KEY = giphyApi.API_KEY = "Your_wonderfull_api_key"
@@ -7,8 +10,7 @@ API_KEY = giphyApi.API_KEY = "Your_wonderfull_api_key"
 # topic to search
 tag = ""
 
-
-# Get random GIF
+# Get GIF
 giphyApi.getGif(API_KEY=API_KEY, tag=tag)
 
 
